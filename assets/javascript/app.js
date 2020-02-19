3f06409a3ddcbbae38df7a7b9a2c09a38f3e347f
Hung Le <hungle913@gmail.com>
2020-02-19 15:37:04 -0800

    linked css and js files to html file. Also create functio for start button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
 
-    // jQuery methods go here...
+    // Need load function for game to start
+
+    $("#start_button").click(function() {
+
+        $("#start_button").html("SUBMIT ANSWERS")
+
+        function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+            function showQuestions(questions, quizContainer){
+                // code will go here
+            }
+        
+            function showResults(questions, quizContainer, resultsContainer){
+                // code will go here
+            }
+        
+            // show the questions
+            showQuestions(questions, quizContainer);
+        
+            // when user clicks submit, show results
+            submitButton.onclick = function(){
+                showResults(questions, quizContainer, resultsContainer);
+            }
+        }
+
+
+
+    });
   
   });
+
